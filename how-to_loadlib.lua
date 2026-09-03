6c7b0241319a4a66d745af290c181591a69cbb88
Baldur <114744176+BaldurRoethingheim@users.noreply.github.com>
2026-09-03 23:40:41 +0200

    path path aesthetics and typo

--- how-to_loadlib.lua
+++ how-to_loadlib.lua
@@ -1,7 +1,7 @@
 -- copy this block at the beginning of every lua file for ease of use AND load_file.lua in same directory
 -------------------------------------------------------------------------
 -------------------------------------------------------------------------
-func, err = loadfile(".\\load_files.lua"); -- relative pathe can be used
+func, err = loadfile("./load_files.lua"); -- relative path can be used
 
 if not func then
     print("\r\nError loading file:\r\n\r\n", err, "\r\n\r\n");
@@ -13,7 +13,7 @@ end
 
 -------------------------------------------------------------------------
 -- load list of lua files and uses their functions (as if it was included like C programming language)
-load_files({".\\lib1.lua", ".\\lib2.lua"--[[does not exist but program continues]]});
+load_files({"./lib1.lua", "./lib2.lua"--[[does not exist but program continues]]});
 -------------------------------------------------------------------------
 -------------------------------------------------------------------------
 
