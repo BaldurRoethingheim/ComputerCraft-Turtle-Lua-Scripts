5f91d1024f1fbbae8381601cf1127807489e76f8
Baldur <114744176+BaldurRoethingheim@users.noreply.github.com>
2026-09-10 01:56:57 +0200

    dir organised
    sorted files into themed folders

---
+++ utils/standardFunctions.lua
@@ -0,0 +1,129 @@
+------------------------------------------------
+-- standard functions 
+------------------------------------------------
+function df()         -- digForward
+  while (turtle.detect()) do
+    turtle.dig();
+  end    
+  turtle.forward();
+end
+function dU()         -- digUp
+  while (turtle.detectUp()) do
+      turtle.digUp();
+  end
+  turtle.up();
+end
+function dD()         -- digDown
+  while (turtle.detectDown()) do
+    turtle.digDown();
+  end
+  turtle.down();
+end
+function f(n)         -- go forward N with digging if block in they way
+  n = n or 2
+  for i=1, n, 1 do
+    df();
+  end
+end
+function b(n)         -- go back N
+  n = n or 2
+  for i=1, n, 1 do
+    turtle.back();
+  end
+end
+function u(n)         -- go up N with digging if block in they way
+  n = n or 1
+  for i=1, n, 1 do
+    dU();
+  end
+end
+function d(n)         -- go down N with digging if block in they way
+  n = n or 1
+  for i=1, n, 1 do
+    dD();
+  end
+end
+function r(n)         -- right sideways N
+  n = n or 2;
+  turtle.turnRight();
+  f(n);
+  turtle.turnLeft();
+end
+function l(n)         -- left sideways N
+  n = n or 2;
+  turtle.turnLeft();
+  f(n);
+  turtle.turnRight();    
+end
+local max_fuel_lvl = 20000; -- the maximum fuel level a turtle can have
+function refuelN(refuel_to_minimum_level)
+  -- found check and set them to the extremes
+  if refuel_to_minimum_level < 1 then
+    refuel_to_minimum_level = 1;
+  elseif refuel_to_minimum_level > max_fuel_lvl then
+    refuel_to_minimum_level = max_fuel_lvl;
+  end
+
+  -- stepswise refuel to the refuel_to_minimum_level
+  for i = 1, 16, 1 do
+    if turtle.getFuelLevel() <= refuel_to_minimum_level then
+      turtle.refuel(1);
+    else
+      return  
+    end  
+  end    
+end
+function pf(BlockName)  -- placeForward
+  BlockName = BlockName or "minecraft:stonebrick";
+  if turtle.detect() == false and searchBlock(BlockName) == true then
+    turtle.place();
+  end
+end
+function pU(BlockName)  -- placeUp
+  BlockName = BlockName or "minecraft:stonebrick";
+  if turtle.detectUp() == false and searchBlock(BlockName) == true then
+    turtle.placeUp();
+  end    
+end
+function pD(BlockName)  -- placeDown
+  BlockName = BlockName or "minecraft:stonebrick";
+  if turtle.detectDown() == false and searchBlock(BlockName) == true then
+    turtle.placeDown();
+  end    
+end
+function swD(Blockname)   -- swapDown
+  Blockname = Blockname or "chisel:concrete";
+  
+  local success, data = turtle.inspectDown();
+  
+  if searchBlock(BlockName) == true then
+    turtle.digDown();
+    turtle.placeDown();        
+  end
+end
+
+function searchBlock(blockname)
+  blockname = blockname or "chisel:concrete";
+  --
+  local d = turtle.getItemDetail();
+  if d and d.name == blockname then
+    print("slot", turtle.getSelectedSlot(), ":", d.name);
+    return true; -- found it & remains at this slot
+  end
+  --
+  for i=1, 16, 1 do
+    turtle.select(i);
+    local data = turtle.getItemDetail();
+    if data then
+      if data.name == blockname then
+        print("slot", i, ":", data.name);
+        return true; -- found it & remains at this slot    
+      end 
+    end
+    if i == 16 then
+      turtle.select(1);
+      print(blockname ," was not found");
+      return false;
+    end
+  end
+end
